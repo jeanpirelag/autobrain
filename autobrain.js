aef6cf107d8e0003a7c8eed35ec0fda2580becd9
Jean J. Pirela <112102882+jeanpirelag@users.noreply.github.com>
2022-12-18 10:56:42 -0400

    Create autobrain.js

---
+++ autobrain.js
@@ -0,0 +1,17 @@
+[
+    {
+        "taskid": "1457116",
+        "guideid": "4575",
+        "jobTitle": "Judge The Sentiment Of Youtube Comment"
+    },
+    {
+        "taskid": "1970178",
+        "guideid": "4575",
+        "jobTitle": "Supplier Locations"
+    },
+    {
+        "taskid": "1652852",
+        "guideid": "4575",
+        "jobTitle": "Format 4 - 5Th Test"
+    }
+]
